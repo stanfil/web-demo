c4ba19ba17189f0873f10f96524057b1078fffcf
stanfil <352070351@qq.com>
2017-12-18 22:54:53 +0800

    index header & add cv

---
+++ public/CV/js/main.js
@@ -0,0 +1,24 @@
+let callback = function(){
+    $('.item-skills').each(function(){
+        newWidth = $(this).parent().width() * $(this).data('percent');
+        $(this).width(0);
+        $(this).animate({
+            width: newWidth,
+        }, 1000);
+    });
+    $('.icons-red').each(function(){
+        height = $(this).height();
+        $(this).animate({
+            height: 14,
+        }, 2000);
+    });
+};
+$(document).ready(callback);
+
+let resize;
+window.onresize = function() {
+    clearTimeout(resize);
+    resize = setTimeout(function(){
+        callback();
+    }, 100);
+};
